7fd8de4c91135989c545dfcb9c02243974aab6bc
VoxanyNet <contact@voxany.net>
2023-11-20 21:06:07 -0500

    remove unneeded imports

--- button/__main__.py
+++ button/__main__.py
@@ -1,8 +1,5 @@
-import discord
-import requests as r
-
 from button import bot
 
 bot = bot.ButtonBot()
 
-bot.run("MTE3NjI4ODYwMzkyODc4OTA4NA.G1j97O.PvMrvWNrD7uo_fjAwgBEPwFseJ-OzptSE38PHk")
+bot.run()
